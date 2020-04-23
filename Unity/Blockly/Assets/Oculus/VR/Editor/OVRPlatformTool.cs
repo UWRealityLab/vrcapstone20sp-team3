fa5e32a6b72ebdd378fb3b907ca38cb834f2ea47
yumatou <yumatou@uw.edu>
2020-04-22 18:07:23 -0700

    add emit, move, delete actions (uses keypresses)

--- Unity/Blockly/Assets/Oculus/VR/Editor/OVRPlatformTool.cs
+++ Unity/Blockly/Assets/Oculus/VR/Editor/OVRPlatformTool.cs
@@ -438,25 +438,16 @@ namespace Assets.Oculus.VR.Editor
 				updateThread.Start();
 			}
 
-			string uploadCommand;
-			if (genUploadCommand(targetPlatform, out uploadCommand))
-			{
-				var thread = new Thread(delegate ()
+			var thread = new Thread(delegate () {
+				// Wait for update process to finish before starting upload process
+				while (activeProcess)
 				{
-					// Wait for update process to finish before starting upload process
-					while (activeProcess)
-					{
-						Thread.Sleep(100);
-					}
-					retryCount = 0;
-					Command(targetPlatform, dataPath, uploadCommand);
-				});
-				thread.Start();
-			}
-			else
-			{
-				UnityEngine.Debug.LogError("Failed to generated upload command.");
-			}
+					Thread.Sleep(100);
+				}
+				retryCount = 0;
+				Command(targetPlatform, dataPath);
+			});
+			thread.Start();
 		}
 
 		private static string CheckForPlatformUtil(string dataPath)
@@ -596,47 +587,51 @@ namespace Assets.Oculus.VR.Editor
 			}
 		}
 
-		static void Command(TargetPlatform targetPlatform, string dataPath, string uploadCommand)
+		static void Command(TargetPlatform targetPlatform, string dataPath)
 		{
 			string platformUtilPath = CheckForPlatformUtil(dataPath);
 
-			activeProcess = true;
-			InitializePlatformUtilProcess(platformUtilPath, uploadCommand);
+			string args;
+			if (genUploadCommand(targetPlatform, out args))
+			{
+				activeProcess = true;
+				InitializePlatformUtilProcess(platformUtilPath, args);
 
-			ovrPlatUtilProcess.Exited += new EventHandler(
-				(s, e) =>
-				{
-					activeProcess = false;
-				}
-			);
+				ovrPlatUtilProcess.Exited += new EventHandler(
+					(s, e) =>
+					{
+						activeProcess = false;
+					}
+				);
 
-			ovrPlatUtilProcess.OutputDataReceived += new DataReceivedEventHandler(
-				(s, e) =>
-				{
-					if (e.Data != null && e.Data.Length != 0 && !e.Data.Contains("\u001b"))
+				ovrPlatUtilProcess.OutputDataReceived += new DataReceivedEventHandler(
+					(s, e) =>
+					{
+						if (e.Data != null && e.Data.Length != 0 && !e.Data.Contains("\u001b"))
+						{
+							OVRPlatformTool.log += e.Data + "\n";
+						}
+					}
+				);
+				ovrPlatUtilProcess.ErrorDataReceived += new DataReceivedEventHandler(
+					(s, e) =>
 					{
 						OVRPlatformTool.log += e.Data + "\n";
 					}
-				}
-			);
-			ovrPlatUtilProcess.ErrorDataReceived += new DataReceivedEventHandler(
-				(s, e) =>
-				{
-					OVRPlatformTool.log += e.Data + "\n";
-				}
-			);
+				);
 
-			try
-			{
-				ovrPlatUtilProcess.Start();
-				ovrPlatUtilProcess.BeginOutputReadLine();
-				ovrPlatUtilProcess.BeginErrorReadLine();
-			}
-			catch
-			{
-				if (ThrowPlatformUtilStartupError(platformUtilPath))
+				try
 				{
-					Command(targetPlatform, dataPath, uploadCommand);
+					ovrPlatUtilProcess.Start();
+					ovrPlatUtilProcess.BeginOutputReadLine();
+					ovrPlatUtilProcess.BeginErrorReadLine();
+				}
+				catch
+				{
+					if (ThrowPlatformUtilStartupError(platformUtilPath))
+					{
+						Command(targetPlatform, dataPath);
+					}
 				}
 			}
 		}
---
+++ Unity/Blockly/Assets/Scripts/BlockController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    public float gridSize;
+    private GameObject selectedBlock;  // currently selected block
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Select();
+        Move(this.selectedBlock);
+    }
+
+    // if mouse click on a block, set that block to be the currently selected block
+    private void Select()
+    {
+        // TODD: update the if condition + Raycast logic to use gestures
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse is down");
+
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit)
+            {
+                Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+                if (hitInfo.collider.gameObject.tag == "Selectable")
+                {
+                    this.selectedBlock = hitInfo.collider.gameObject;
+                    Debug.Log("It's working!");
+                }
+                else
+                {
+                    Debug.Log("nopz");
+                }
+            }
+            else
+            {
+                Debug.Log("No hit");
+            }
+        }
+    }
+
+    // if key press for move/delete, perform action on the currently selected block
+    // does nothing if no block is currently selected
+    private void Move(GameObject gameObject)
+    {
+        if (gameObject == null)
+        {
+            return;
+        }
+
+        // TODO: update the if conditions to be based on gestures rather than keypresses
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 position = gameObject.transform.position;
+            position.x -= gridSize;
+            gameObject.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Vector3 position = gameObject.transform.position;
+            position.x += gridSize;
+            gameObject.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 position = gameObject.transform.position;
+            position.y -= gridSize;
+            gameObject.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Vector3 position = gameObject.transform.position;
+            position.y += gridSize;
+            gameObject.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Vector3 position = gameObject.transform.position;
+            position.z -= gridSize;
+            gameObject.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Vector3 position = gameObject.transform.position;
+            position.z += gridSize;
+            gameObject.transform.position = position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
