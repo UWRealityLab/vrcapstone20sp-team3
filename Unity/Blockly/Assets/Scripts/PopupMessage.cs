c30128673be26ea27bae5a6bf1912d1bf28a3df9
Sea Lee <seal14@cs.washington.edu>
2020-05-26 23:00:36 -0700

    added popup stuff when clicking levels

---
+++ Unity/Blockly/Assets/Scripts/PopupMessage.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupMessage : MonoBehaviour
+{
+    public GameObject ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clickButton();
+    }
+
+    public void Open(string message)
+    {
+        ui.SetActive(!ui.activeSelf);
+        if (ui.activeSelf)
+        {
+            if (!string.IsNullOrEmpty(message))
+            {
+                Text textObject = ui.gameObject.GetComponentInChildren<Text>();
+                textObject.text = message;
+            }
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void Close()
+    {
+        ui.SetActive(!ui.activeSelf);
+    }
+
+    private void clickButton()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Button okButton = GameObject.Find("OK").GetComponent<Button>();
+            okButton.onClick.Invoke();
+        }
+    }
+}
