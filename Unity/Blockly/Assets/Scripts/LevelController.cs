6952aaea862aeb54b29a94ba98c217f249e70411
Sea Lee <seal14@cs.washington.edu>
2020-05-31 12:25:26 -0700

    merged Erik's stuff

--- Unity/Blockly/Assets/Scripts/LevelController.cs
+++ Unity/Blockly/Assets/Scripts/LevelController.cs
@@ -11,17 +11,22 @@ public class LevelController : MonoBehaviour
     private GameObject level2Button;
     private GameObject level3Button;
 
-    public GameObject popup;
-    private PopupMessage popupMessage;
+    public GameObject levelPopup;
+    private LevelPopupController popupController;
+
+    //public GameObject popup;
+    //private PopupMessage popupMessage;
 
     // Start is called before the first frame update
     void Start()
     {
-        popupMessage = popup.GetComponent<PopupMessage>();
+        popupController = levelPopup.GetComponent<LevelPopupController>();
+        //popupMessage = popup.GetComponent<PopupMessage>();
         level1Button = GameObject.Find("Level 1 Button");
         level2Button = GameObject.Find("Level 2 Button");
         level3Button = GameObject.Find("Level 3 Button");
-        popupMessage.Close();
+        //popupMessage.Close();
+        popupController.Close();
     }
 
     // Update is called once per frame
@@ -39,34 +44,31 @@ public class LevelController : MonoBehaviour
 
     public void BeginLevel1()
     {
-        string dir = "Introduction to gestures\n";
-        dir += "1. Move a block to the right.\nPoint your pointer finger and drag in desired direction.\n";
-        dir += "2. Emit block at cursor.\nStart with a fist and release,\nmaking a 5 with your fingers, hand facing down.";
-        popupMessage.Open(dir);
-        SetButtonActive(false);
-        LoadScene();
+        //string dir = "Introduction to gestures\n";
+        //dir += "1. Move a block to the right.\nPoint your pointer finger and drag in desired direction.\n";
+        //dir += "2. Emit block at cursor.\nStart with a fist and release,\nmaking a 5 with your fingers, hand facing down.";
+        SetupLevel(-1);
     }
 
     public void BeginLevel2()
     {
-        string dir = "Module\nTap on recording button to start recording your module.\n";
-        dir += "Emit a block and move cursor to the right.\n Finish by pressing recording button again.";
-        popupMessage.Open(dir);
-        SetButtonActive(false);
-        LoadScene();
+        //string dir = "Module\nTap on recording button to start recording your module.\n";
+        //dir += "Emit a block and move cursor to the right.\n Finish by pressing recording button again.";
+        SetupLevel(0);
     }
 
     public void BeginLevel3()
     {
-        string dir = "Loop\nChoose module and loop over it ten times in a clockwise circular motion.\n";
-        dir += "Apply module to main grid.";
-        popupMessage.Open(dir);
-        SetButtonActive(false);
-        LoadScene();
+        //string dir = "Loop\nChoose module and loop over it ten times in a clockwise circular motion.\n";
+        //dir += "Apply module to main grid.";
+        SetupLevel(1);
     }
 
-    private void LoadScene()
+    private void SetupLevel(int level)
     {
+        popupController.SetLevel(level);
+        popupController.Open("Welcome to Blockly Puzzle Mode", "Press <TODO> to continue", null);
+        SetButtonActive(false);
         SceneManager.LoadScene("Blockly", LoadSceneMode.Additive);
         //SceneManager.LoadScene("GestureRecognition", LoadSceneMode.Additive);
     }
---
+++ Unity/Blockly/Assets/Scripts/LevelPopupController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelPopupController : MonoBehaviour
+{
+    public GameObject ui;
+    private PuzzleController puzzleController;
+    private bool verified;
+    private int currentLevel;
+
+    private List<string> titles;
+    private List<string> instructions;
+
+    public string INSTRUCTION = "Fill in the green outline with blocks. Place cursor at yellow box to submit.";
+
+    public string NEXT_LEVEL_TITLE = "LEVEL PASSED!";
+    public string NEXT_LEVEL_INST = "Make a <TODO> gesture to move to the next level!";
+
+    public string CONGRATULATIONS = "CONGRATULATIONS!";
+    public string END_INST = " You will now enter free play mode, where you can explore your creativity in 3D :O";
+
+    public string LEVEL1_TITLE = "LEVEL 1: INTRODUCTION";
+    public string LEVEL1_INST = "To move block, point your pointer finger and drag in desired direction." +
+        "To emit block at cursor, start with a fist and release,\nmaking a 5 with your fingers, hand facing down.";
+
+    public string LEVEL2_TITLE = "LEVEL 2: MODULE";
+    public string LEVEL2_INST = "To create a module, <TODO>.";
+    public string LEVEL2_MODULE_INST = "All of the modules you define in this level will be available to you in the" +
+        "module library for use in future levels. Good luck!";
+
+    public string LEVEL3_TITLE = "LEVEL 3: LoOpY StAirS";
+    public string LEVEL3_INST = "To loop, select module by <TODO> and" +
+        "loop over it desired number of times in a clockwise circular motion. Apply module by <TODO>.";
+
+     void Start()
+    {
+        currentLevel = -1;
+        verified = true;
+        titles = new List<string>();
+        instructions = new List<string>();
+
+        titles.Add(LEVEL1_TITLE);
+        titles.Add(LEVEL2_TITLE);
+        titles.Add(LEVEL3_TITLE);
+
+        instructions.Add(LEVEL1_INST);
+        instructions.Add(LEVEL2_INST);
+        instructions.Add(LEVEL3_INST);
+    }
+
+    private void Update()
+    {
+        closePopup();
+        if (puzzleController != null && puzzleController.UserSubmitted() && !verified)
+        {
+            MoveToNextLevel();
+        }
+    }
+
+    public void SetLevel(int level)
+    {
+        this.currentLevel = level;
+    }
+
+    public void Open(string title, string instruction, string module)
+    {
+        ui.SetActive(!ui.activeSelf);
+        if (ui.activeSelf)
+        {
+            setText(title, 0);
+            setText(instruction, 1);
+            setText(module, 2);
+
+        }
+    }
+
+    public void Close()
+    {
+        ui.SetActive(!ui.activeSelf);
+
+        if (puzzleController == null)
+        {
+            puzzleController = GameObject.Find("Puzzle Controller").GetComponent<PuzzleController>();
+        }
+
+        if (verified)
+        {
+            if (currentLevel >= 0)
+            {
+                puzzleController.ClearGrid();
+            }
+            if (puzzleController.VerifyPuzzleId(currentLevel + 1))
+            {
+                currentLevel++;
+                if (currentLevel == 1)
+                {
+                    Open(titles[currentLevel], instructions[currentLevel], INSTRUCTION + LEVEL2_MODULE_INST);
+                } else
+                {
+                    Open(titles[currentLevel], instructions[currentLevel], INSTRUCTION);
+                }
+                puzzleController.StartPuzzle(currentLevel);
+                verified = false;
+            } else
+            {
+                Open(CONGRATULATIONS, END_INST, null);
+                verified = false;
+            }
+        }
+    }
+
+    public void MoveToNextLevel()
+    {
+        if (puzzleController.VerifyPuzzle())
+        {
+            verified = true;
+            Open(NEXT_LEVEL_TITLE, NEXT_LEVEL_INST, null);
+        }
+    }
+
+    private void closePopup()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Close();
+        }
+    }
+
+    private void setText(string message, int childNum)
+    {
+        if (!string.IsNullOrEmpty(message))
+        {
+            Text textObject = ui.transform.GetChild(childNum).gameObject.GetComponentInChildren<Text>();
+            textObject.text = message;
+        }
+    }
+}
