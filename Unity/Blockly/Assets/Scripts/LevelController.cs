6c08de35bc29eb8a7bf4cfe64cae3d85667fb801
Sea Lee <seal14@cs.washington.edu>
2020-06-01 21:19:22 -0700

    i think i broke it more actually but im rly super hungry

--- Unity/Blockly/Assets/Scripts/LevelController.cs
+++ Unity/Blockly/Assets/Scripts/LevelController.cs
@@ -36,46 +36,32 @@ public class LevelController : MonoBehaviour
     public void SetButtonActive(bool active)
     {
         level1Button.SetActive(active);
-            UnityEngine.Debug.Log("set level1 button to " + active);
         level2Button.SetActive(active);
         level3Button.SetActive(active);
     }
 
     public void BeginLevel1()
     {
-            UnityEngine.Debug.Log("in begin level 1");
-        string dir = "Introduction to gestures\n";
-        dir += "1. Move a block to the right.\nPoint your pointer finger and drag in desired direction.\n";
-        dir += "2. Emit block at cursor.\nStart with a fist and release,\nmaking a 5 with your fingers, hand facing down.";
-        popupMessage.Open(dir);
-        SetButtonActive(false);
-        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
-        }
+        SetupLevel(-1);
+    }
 
     public void BeginLevel2()
     {
-        string dir = "Module\nTap on recording button to start recording your module.\n";
-        dir += "Emit a block and move cursor to the right.\n Finish by pressing recording button again.";
-        popupMessage.Open(dir);
-        SetButtonActive(false);
-        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+        SetupLevel(0);
     }
 
     public void BeginLevel3()
     {
-        string dir = "Loop\nChoose module and loop over it ten times in a clockwise circular motion.\n";
-        dir += "Apply module to main grid.";
-        popupMessage.Open(dir);
-        SetButtonActive(false);
-        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
-
+        SetupLevel(-1);
     }
 
-    //private void LoadScene()
-    //{
-    //    SceneManager.LoadScene("Blockly", LoadSceneMode.Additive);
-    //    //SceneManager.LoadScene("GestureRecognition", LoadSceneMode.Additive);
-    //}
-}
+    private void SetupLevel(int level)
+    {
+        popupMessage.SetLevel(level);
+        popupMessage.Open("Welcome to Blockly Puzzle Mode");
+        SetButtonActive(false);
+        SceneManager.LoadScene("Environment", LoadSceneMode.Additive);
+    }
+ }
 
 }
