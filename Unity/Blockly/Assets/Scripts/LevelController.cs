49238621c25b7a5a965b1d6a60b2950969550ea7
Sea Lee <seal14@cs.washington.edu>
2020-06-07 17:57:34 -0700

    levels pop up i think

--- Unity/Blockly/Assets/Scripts/LevelController.cs
+++ Unity/Blockly/Assets/Scripts/LevelController.cs
@@ -19,17 +19,58 @@ public class LevelController : MonoBehaviour
   [SerializeField] [NotNull]
     private GameObject level3Button;
 
-        public const string WELCOME_TITLE = "Welcome to Blockly Puzzle Mode";
-        public const string WELCOME_INSTR = "Pinch to get rid of this popup!";
+    private int currentLevel;
+    private bool verified;
+    private bool levelSet;
+
+    public const string WELCOME_TITLE = "Welcome to Blockly Puzzle Mode";
+    public const string WELCOME_INST = "Pinch to get rid of this popup!";
+
+    public const string INSTRUCTION = "Fill in the green outline with blocks. Place cursor at yellow box to submit.";
+
+    public const string NEXT_LEVEL_TITLE = "LEVEL PASSED!\n";
+    public const string NEXT_LEVEL_INST = "Make a pinch gesture to move to the next level!";
+
+    public const string CONGRATS_TITLE = "CONGRATULATIONS!";
+    public const string CONGRATS_INST = " You will now enter free play mode, where you can explore your creativity in 3D!";
+
+    public const string LEVEL1_TITLE = "LEVEL 1: Introductory Statements";
+    public const string LEVEL1_INST = "To move block, point your pointer finger and drag in desired direction.\n" +
+    "To emit block at cursor, start with a fist and release, making a 5 with your fingers, hand facing down.\n\n";
+
+    public const string LEVEL2_TITLE = "LEVEL 2: MODULE";
+    public const string LEVEL2_INST = "To create a module, fold your hand with only thumb on bottom for both hands.\n\n";
+    public const string LEVEL2_MODULE_INST = "All of the modules you define in this level will be available to you in the" +
+        "module library for use in future levels. Good luck!";
+
+    public const string LEVEL3_TITLE = "LEVEL 3: LoOpY StAirS";
+    public const string LEVEL3_INST = "To loop, select module by making a pinch gesture and " +
+    "loop over it desired number of times in a clockwise circular motion.\n" +
+    "Apply module by dragging pinched module from module library to the play area.\n\n";
+
+    private List<string> titles;
+    private List<string> instructions;
 
     void Awake() {
         Debug.Assert(Instance == null, "singleton class instantiated multiple times");
         Instance = this;
+
+        titles = new List<string>();
+        titles.Add(LEVEL1_TITLE);
+        titles.Add(LEVEL2_TITLE);
+        titles.Add(LEVEL3_TITLE);
+
+        instructions = new List<string>();
+        instructions.Add(LEVEL1_INST + INSTRUCTION);
+        instructions.Add(LEVEL2_INST + LEVEL2_MODULE_INST);
+        instructions.Add(LEVEL3_INST + INSTRUCTION);
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        currentLevel = -1;
+        verified = false;
         PopupMessage.Instance.ui.SetActive(false);
         SetButtonActive(false);
     }
@@ -37,7 +78,10 @@ public class LevelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (PuzzleController.Instance != null && !verified)
+        {
+            MoveToNextLevel();
+        }
     }
 
     public void SetButtonActive(bool active)
@@ -49,24 +93,56 @@ public class LevelController : MonoBehaviour
 
     public void BeginLevel1()
     {
-        SetupLevel(-1);
+        StartLevel(0);
     }
 
     public void BeginLevel2()
     {
-        SetupLevel(0);
+        StartLevel(1);
     }
 
     public void BeginLevel3()
     {
-        SetupLevel(1);
+        StartLevel(2);
     }
 
-    private void SetupLevel(int level)
+    public void MoveToNextLevel()
     {
-        PopupMessage.Instance.SetLevel(level);
-        PopupMessage.Instance.Open(WELCOME_TITLE, WELCOME_INSTR);
+        if (PuzzleController.Instance.UserSubmittedCorrect())
+        {
+            verified = true;
+            currentLevel++;
+            if (PuzzleController.Instance.VerifyPuzzleId(currentLevel))
+            {
+                this.levelSet = false;
+                PopupMessage.Instance.Open(NEXT_LEVEL_TITLE, NEXT_LEVEL_INST);
+            }
+            else
+            {
+                PopupMessage.Instance.Open(CONGRATS_TITLE, CONGRATS_INST);
+                PuzzleController.Instance.ClearGrid();
+            }
+        }
+    }
+
+    private void StartLevel(int level)
+    {
+        this.currentLevel = level;
+        this.levelSet = false;
+        PopupMessage.Instance.Open(WELCOME_TITLE, WELCOME_INST);
         SetButtonActive(false);
+    }
+
+    public void SetupLevel()
+    {
+        if (!levelSet && PuzzleController.Instance.VerifyPuzzleId(currentLevel))
+        {
+            PopupMessage.Instance.Open(titles[currentLevel], instructions[currentLevel]);
+            PuzzleController.Instance.ClearGrid();
+            PuzzleController.Instance.StartPuzzle(currentLevel);
+            levelSet = true;
+            verified = false;
+        }
     }
  }
 
--- Unity/Blockly/Assets/Scripts/PopupMessage.cs
+++ Unity/Blockly/Assets/Scripts/PopupMessage.cs
@@ -13,131 +13,55 @@ public class PopupMessage : MonoBehaviour
 
   [NotNull]
     public GameObject ui;
+    
 
-    private bool verified;
-    private int currentLevel;
-
-    public const string INSTRUCTION = "Fill in the green outline with blocks. Place cursor at yellow box to submit.";
-
-    public const string NEXT_LEVEL_TITLE = "LEVEL PASSED!";
-    public const string NEXT_LEVEL_INST = "Make a pinch gesture to move to the next level!";
-
-    public const string CONGRATS_TITLE = "CONGRATULATIONS!";
-    public const string CONGRATS_INST = " You will now enter free play mode, where you can explore your creativity in 3D!";
-
-    public const string LEVEL1_TITLE = "LEVEL 1: Introductory Statements";
-    public const string LEVEL1_INST = "To move block, point your pointer finger and drag in desired direction.\n" +
-    "To emit block at cursor, start with a fist and release, making a 5 with your fingers, hand facing down.\n\n";
-
-    public const string LEVEL2_TITLE = "LEVEL 2: MODULE";
-    public const string LEVEL2_INST = "To create a module, fold your hand with only thumb on bottom for both hands.\n\n";
-    public const string LEVEL2_MODULE_INST = "All of the modules you define in this level will be available to you in the" +
-        "module library for use in future levels. Good luck!";
-
-    public const string LEVEL3_TITLE = "LEVEL 3: LoOpY StAirS";
-    public const string LEVEL3_INST = "To loop, select module by making a pinch gesture and " +
-    "loop over it desired number of times in a clockwise circular motion.\n" +
-    "Apply module by dragging pinched module from module library to the play area.\n\n";
-
-
-        private List<string> titles;
-    private List<string> instructions;
 
   void Awake() {
     Debug.Assert(Instance == null, "singleton class instantiated multiple times");
     Instance = this;
-
-        currentLevel = -1;
-        verified = true;
-
-        titles = new List<string>();
-        titles.Add(LEVEL1_TITLE);
-        titles.Add(LEVEL2_TITLE);
-        titles.Add(LEVEL3_TITLE);
-
-        instructions = new List<string>();
-        instructions.Add(LEVEL1_INST + INSTRUCTION);
-        instructions.Add(LEVEL2_INST + LEVEL2_MODULE_INST);
-        instructions.Add(LEVEL3_INST + INSTRUCTION);
   }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        }
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.B))
         {
-          ClickButton();
+            ClickButton();
         }
-        if (PuzzleController.Instance != null && !verified)
-        {
-            MoveToNextLevel();
-        }
-    }
 
-    public void SetLevel(int level)
-    {
-        this.currentLevel = level;
     }
 
     public void Open(string title, string instruction)
     {
-        ui.SetActive(!ui.activeSelf);
-        if (ui.activeSelf)
-        {
+        ui.SetActive(true);
+        
+/*        if (ui.activeSelf)
+        {*/
             setText(title, 0);
             setText(instruction, 1);
-        }
+/*        }*/
     }
 
     public void Close()
     {
-        ui.SetActive(!ui.activeSelf);
-
-        if (verified)
-        {
-            if (currentLevel >= 0)
-            {
-                PuzzleController.Instance.ClearGrid();
-            }
-            if (PuzzleController.Instance.VerifyPuzzleId(currentLevel + 1))
-            {
-                currentLevel++;
-                Open(titles[currentLevel], instructions[currentLevel]);
-                PuzzleController.Instance.StartPuzzle(currentLevel);
-                verified = false;
-            } // an else which means they are completed with all possible levels --> give them a congratulations :D
-        }
+        ui.SetActive(false);
+        LevelController.Instance.SetupLevel();
+        
     }
 
     public void ClickButton()
     {
-      Button okButton = GameObject.Find("OK").GetComponent<Button>();
-      okButton.onClick.Invoke();
+        Button okButton = GameObject.Find("OK").GetComponent<Button>();
+        okButton.onClick.Invoke();
     }
 
-    public void MoveToNextLevel()
-    {
-        if (PuzzleController.Instance.UserSubmittedCorrect())
-        {
-            verified = true;
-            if (PuzzleController.Instance.VerifyPuzzleId(currentLevel + 1))
-            {
-                Open(NEXT_LEVEL_TITLE, NEXT_LEVEL_INST);
-            }
-            else
-            {
-                Open(CONGRATS_TITLE, CONGRATS_INST);
-            }
-        }
-    }
-
-    private void setText(string message, int childNum)
+        private void setText(string message, int childNum)
     {
         if (message != null)
         {
